80bf247586f182803f1c6b3986d6d7a6bc49f06e
Kevin Dang <k3dang@ucsd.edu>
2017-11-05 20:09:36 -0800

    added fragment page for adding goals. Attached to current goals activity

---
+++ app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/GoalFragment.java
@@ -0,0 +1,21 @@
+package edu.ucsd.calab.usingextrasensorylabels;
+
+import android.content.Context;
+import android.net.Uri;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GoalFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.goal_fragment_layout, container, false);
+    }
+}
