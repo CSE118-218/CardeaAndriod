29d92d0021c9ee2841e8ceafb2176354640b5cfe
Kevin Dang <k3dang@ucsd.edu>
2017-11-17 13:38:40 -0800

    Added simple progress bar

--- app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/ProgressActivity.java
+++ app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/ProgressActivity.java
@@ -2,6 +2,9 @@ package edu.ucsd.calab.usingextrasensorylabels;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 /**
  * Created by UsernameChecksOut on 11/5/17.
@@ -9,10 +12,26 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ProgressActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+    private Button button;
+    private int prog = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progress_page);
+
+        button = (Button) findViewById(R.id.progressButton);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar2);
+        progressBar.setMax(100);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setProgress(prog);
+                prog = prog+5;
+            }
+        });
     }
 
 }
