0d6009b15fe1c945b44ba860b82a8d17e85d17b6
zht <alexander.ht.zhang@gmail.com>
2017-11-30 00:41:48 -0800

    Finished dynamic walking recommender system, added two cases

--- app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/MainActivity.java
+++ app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/MainActivity.java
@@ -1,14 +1,18 @@
 package edu.ucsd.calab.usingextrasensorylabels;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -18,19 +22,31 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
 
-
-
+    private TextView test;
+    private int mInterval = 1000; // 5 seconds by default, can be changed later
+    private Handler mHandler;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
         setContentView(R.layout.main_layout);
+
+//        set text for animated recommender text
+//        test = (TextView)findViewById(R.id.recommender);
+        // animate the stroke tips in the goal page
+  // Set focus to the textview
+
+        mHandler = new Handler();
+        startRepeatingTask();
+        //        read message
 
 
         final Button cognitiveButton = (Button) findViewById(R.id.cognitiveButton);
         final Button motorSkillsButton = (Button) findViewById(R.id.motorSkillsButton);
         final Button lifeStyleButton = (Button) findViewById(R.id.lifeStyleButton);
+
+
 
         cognitiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,5 +124,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                test = (TextView)findViewById(R.id.recommender);
+                registermessage(); //this function can change value of mInterval.
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
+    }
+
+    public void registermessage() {
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        String walkingtime = sharedPreferences.getString("Walking", "0");
+        int result = Integer.parseInt(walkingtime);
+        if (result >= 15) {
+            test.setText("Walk to 64 Degree to eat?");
+        } else if((result <= 15) && (result > 1)) {
+            test.setText("Want a cup coffee from Price Center? ");
+        } else { test.setText("No goal has been setted");
+
+        }
     }
 }
