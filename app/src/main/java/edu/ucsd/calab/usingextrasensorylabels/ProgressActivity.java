02ef3607d67460f8f93e9ad4cb37f259a819c315
zht <alexander.ht.zhang@gmail.com>
2017-12-07 15:46:41 -0800

    MPAndroidChart API intergraded into the progress_page

--- app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/ProgressActivity.java
+++ app/src/main/java/edu/ucsd/calab/usingextrasensorylabels/ProgressActivity.java
@@ -1,8 +1,10 @@
 package edu.ucsd.calab.usingextrasensorylabels;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
@@ -18,6 +20,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 
@@ -143,6 +149,65 @@ public class ProgressActivity extends AppCompatActivity {
                 update();
             }
         });
+
+
+//        Chart for weekly progress comparision
+        RadarChart chart = (RadarChart) findViewById(R.id.chart);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(5f, 1));
+        entries.add(new Entry(2f, 2));
+        entries.add(new Entry(7f, 3));
+        entries.add(new Entry(6f, 4));
+        entries.add(new Entry(5f, 5));
+
+        ArrayList<Entry> entries2 = new ArrayList<>();
+        entries2.add(new Entry(1f, 0));
+        entries2.add(new Entry(5f, 1));
+        entries2.add(new Entry(6f, 2));
+        entries2.add(new Entry(3f, 3));
+        entries2.add(new Entry(4f, 4));
+        entries2.add(new Entry(8f, 5));
+
+        RadarDataSet dataset_comp1 = new RadarDataSet(entries, "Company1");
+
+        RadarDataSet dataset_comp2 = new RadarDataSet(entries2, "Company2");
+
+        dataset_comp1.setColor(Color.CYAN);
+        dataset_comp1.setDrawFilled(true);
+
+        dataset_comp2.setColor(Color.RED);
+        dataset_comp2.setDrawFilled(true);
+
+
+        ArrayList<RadarDataSet> dataSets = new ArrayList<RadarDataSet>();
+        dataSets.add(dataset_comp1);
+        dataSets.add(dataset_comp2);
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Communication");
+        labels.add("Technical Knowledge");
+        labels.add("Team Player");
+        labels.add("Meeting Deadlines");
+        labels.add("Problem Solving");
+        labels.add("Punctuality");
+
+
+
+
+        RadarData data = new RadarData(labels, dataSets);
+        chart.setData(data);
+        String description = "Employee-Skill Analysis (scale of 1-10), 10 being the highest";
+        chart.setDescription(description);
+        chart.setWebLineWidthInner(0.5f);
+        chart.setDescriptionColor(Color.RED);
+
+        //chart.setSkipWebLineCount(10);
+        chart.invalidate();
+        chart.animate();
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 
 
@@ -181,6 +246,7 @@ public class ProgressActivity extends AppCompatActivity {
 
         queue.add(jsonArrayRequest);
     }
+
 
 
 
